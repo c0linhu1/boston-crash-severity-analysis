027bea46448e04f3cf57259a26e3aa3d0aac5a85
c0linhu1 <141664358+c0linhu1@users.noreply.github.com>
2026-04-12 17:01:13 -0400

    .

--- grouped_bar_plot.py
+++ grouped_bar_plot.py
@@ -126,7 +126,7 @@ def create_grouped_bar():
         annotations=[
             dict(
                 text='Road Surface:',
-                x=0.01, y=1.13,
+                x=0.01, y=1.05,
                 xref='paper', yref='paper',
                 showarrow=False,
                 font=dict(size=10, color='#555'),
